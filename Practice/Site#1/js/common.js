de81778621e3bcc174531a848e1580a74961e581
name <dont@have.com>
2016-04-14 03:09:50 +0300

    Finishe Page#1 Practice

--- Practice/Site#1/js/common.js
+++ Practice/Site#1/js/common.js
@@ -2,3 +2,18 @@ $(document).ready(function(){
   $('.slider').bxSlider();
 });
 
+$(document).ready(function(){
+	var touch = $('#touch-menu');
+	var menu = $('.nav');
+
+	$(touch).on('click', function(e){
+		e.preventDefault();
+		menu.slideToggle();
+	});
+	$(window).resize(function(){
+		var wid = $(window).width();
+		if(wid > 760 && menu.is(':hidden')) {
+			menu.removeAttr('style');
+		}
+	});
+});
