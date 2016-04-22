c7fc62149e1e8a42af318fd00e737eb8de9abef9
name <dont@have.com>
2016-04-22 16:57:50 +0300

    Site # 3

---
+++ Practice/Site#3/js/common.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+	//Каруселька
+	//Документация: http://owlgraphic.com/owlcarousel/
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:false,
+    responsive:{
+
+        0:{items:0},
+        600:{items:0},
+        1000:{items:3}
+
+    }
+});
+
+
+	// Фикмированная шапка при скролле
+
+$(".hided_menu").removeAttr("style").fadeIn('fast');
+$(".hided_menu").attr({style: "display:none"});
+		$(window).scroll(function(){
+		if ($(this).scrollTop() > 20) {
+			$(".hided_menu").addClass("default").fadeIn('fast');
+			} else {
+			$(".hided_menu").attr({style: "display:none"});
+
+			
+		};
+	});
+			 
+});
+
+	
